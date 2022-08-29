9dff71cf31cf6e93f9333a184457bed0f227b78e
kawinnat09 <Kwinnathmayura@gmail.com>
2022-08-29 17:36:06 +0700

    first commit

--- index.js
+++ index.js
@@ -2,31 +2,50 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const path = require('path')
 
 const app = express()
 const morgan = require('morgan')
 
 const PORT = process.env.PORT || 4000
 
-//database connection
+// database connection
 mongoose.connect(process.env.DB_URI,{useNewUrlParser:true})
 const db = mongoose.connection;
 db.on("error",(error)=>{
     console.log(error)
 })
+
 db.once("open",()=>{
-    console.log("Connected to the databases")
+    console.log("Connect to the databases")
 })
 
-app.use(morgan('dev'))
-
-//http://localhost:4000/
-app.get('/', (req, res) => {
-    return res.json({
-        message:"Homepage"
-    })
+// middleware
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(session({secret:"my secret key",saveUninitialized:true, resave:false }))
+app.use((req,res,next)=>{
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
 })
 
-app.listen(PORT, () => {
+// app.use(morgan('dev'))
+
+// // // http://localhost${PORT}
+// // app.get('/',(req,res)=>{
+// //     // return res.send("Home page")
+// //     return res.json({
+// //         message: "Home page"
+// //     })
+// // })
+
+// // set template engine
+app.use(express.static(path.join(__dirname,"node_modules/bootstrap/dist/")))
+app.set("view engine","ejs")
+
+app.use("", require('./routes/routes'))
+
+app.listen(PORT,(req,res)=>{
     console.log(`Server running at http://localhost:${PORT}`)
 })
---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+const { render } = require('ejs')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    return res.render("index", { title: "Home Page" })
+})
+
+router.get('/users', (req, res) => {
+    return res.status(200).json({
+        message: "All Users"
+    })
+})
+
+module.exports = router
