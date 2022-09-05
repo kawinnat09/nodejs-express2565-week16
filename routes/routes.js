76c92aee8b3737f435e4f5e100e7afb196786f9a
kawinnat09 <Kwinnathmayura@gmail.com>
2022-09-05 16:15:08 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
-const { render } = require('ejs')
 const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
     return res.render("index", { title: "Home Page" })
+})
+
+router.get("/add", (req, res) => {
+    res.render("add_users", { title: "Add Users" })
 })
 
 router.get('/users', (req, res) => {
